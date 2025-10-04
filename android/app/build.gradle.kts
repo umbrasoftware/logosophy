20f6a8512a9f1d8c099d8632e7b2a0902662d729
Leandro Monteiro <neofahrenheituk@gmail.com>
2025-10-04 20:34:13 -0300

    fix: startup crash on release Bump:build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,6 +55,9 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = true 
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Mantém a MainActivity do Flutter, impedindo que o R8 a remova.
+-keep class br.com.umbrasoftware.logosophy.MainActivity { *; }
