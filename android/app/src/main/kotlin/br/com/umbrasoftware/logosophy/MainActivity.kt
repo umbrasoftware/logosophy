c1ca95e958bb73f0b8529ae08b348ea95e8b3f95
Leandro Monteiro <neofahrenheituk@gmail.com>
2025-10-05 00:00:02 -0300

    fix: trying to solve startup bug

---
+++ android/app/src/main/kotlin/br/com/umbrasoftware/logosophy/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.umbrasoftware.logosophy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/logosophy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/logosophy/MainActivity.kt
@@ -1,5 +1,9 @@
+// This file is obsolete. The app's MainActivity has been moved to
+// package br.com.umbrasoftware.logosophy to match the applicationId.
+// Keeping this placeholder to avoid accidental class duplication during builds.
+
 package com.example.logosophy
 
-import io.flutter.embedding.android.FlutterActivity
+// NOTE: Do not define a MainActivity class here. See
+// android/app/src/main/kotlin/br/com/umbrasoftware/logosophy/MainActivity.kt
 
-class MainActivity : FlutterActivity()
